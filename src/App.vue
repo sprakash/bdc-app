91c47e85fcb1ed417349190f4b30aa2c165989fb
sprakash <prakashnitza@gmail.com>
2024-01-07 08:08:01 -0800

    header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
+  <HeaderNavigation />
   <div v-if="records">
     <h1>Airtable Records</h1>
     <ul>
       <li v-for="record in records" :key="record" class="m-5 p-3 bg-blue-100">
         <a
           :href="record.fields.Website"
+          target="_blank"
           class="border-1 border-blue-950 hover:border-red-400 px-3 py-2 text-lg font-bold"
         >
           {{ record.fields.Name }}
@@ -25,19 +27,17 @@
 </template>
 
 <script>
+import HeaderNavigation from "@/components/HeaderNavigation.vue";
 import { ref } from "vue";
 
 export default {
   setup() {
     const records = ref(null);
     const error = ref(null);
-
     const fetchRecords = async () => {
       try {
         const url = `http://localhost:3000/airtable-records`;
-
         console.log(" URL ", url);
-
         const response = await fetch(url);
         const data = await response.json();
         records.value = data.records;
@@ -46,10 +46,9 @@ export default {
         error.value = error.message;
       }
     };
-
     fetchRecords();
-
     return { records, error };
   },
+  components: { HeaderNavigation },
 };
 </script>
---
+++ src/components/HeaderNavigation.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="flex">
+      <img src="@/assets/hires-logo-bdc.png" class="logo" />
+      <ul class="mainMenu flex">
+        <li>Home</li>
+        <li>Filmmakers</li>
+        <li>Films</li>
+        <li>Donate</li>
+        <li>FAQs</li>
+      </ul>
+      <ul class="connect flex">
+        <li>fb icon</li>
+        <li>twitter icon</li>
+        <li>insta</li>
+        <li>youtube</li>
+        <li>linkedin</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  width: 10%;
+  height: 10%;
+  display: block;
+}
+</style>
