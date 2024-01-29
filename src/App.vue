6b255ae6e72d1e2b0e638a888884f356c6867d21
saiyidatul <saiyidatul123@gmail.com>
2024-01-29 22:08:42 +0800

    Added accordion to homepage

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,15 @@
     <!-- <div class="font-bold text-sm text-blue-200">Common header content</div> -->
 
     <div v-if="$route.path === '/'">
-      <section>
-        <h1 class="title">BLACK DOCUMENTARY COLLECTIVE</h1>
-        <p>
+      <section class="centered-content-1">
+        <h1 class="header text-black font-semibold mb-5">
+          BLACK DOCUMENTARY COLLECTIVE
+        </h1>
+        <p class="content-text-1 mb-1">
           "I want to do with the documentary what jazz musicians did with
-          music." - St, Clair Bourne, Founder
+          music."
         </p>
+        <p class="content-text-1 mb-5">- St, Clair Bourne, Founder</p>
 
         <v-carousel cycle show-arrows="hover" hide-delimiters>
           <v-carousel-item src="./assets/gallery1.jpeg" cover></v-carousel-item>
@@ -23,68 +26,112 @@
         </v-carousel>
       </section>
 
-      <section>
-        <h1>Mission</h1>
-        <p>
+      <section class="centered-content mb-12 px-16">
+        <h1 class="header text-black font-bold mb-5">Mission</h1>
+        <p class="content-text-2 text-center px-5 mb-5">
           Dedicated to making a difference in the ever-increasing communities
           that are reflected in and resonate with the varied work of our
           filmmakers, through community partnerships, impact and engagement.
         </p>
-        <p>
+        <p class="content-text-2 text-center px-5 mb-5">
           We have helped countless members raise funds, promote and screen their
           films, connect with organizational and local partners, and secure
           distribution, work-for-hire, consulting, teaching, and more.
         </p>
       </section>
 
-      <hr />
+      <hr class="" />
 
-      <section>
-        <h1>Latest News</h1>
+      <section class="centered-content mb-12 mt-12">
+        <h1 class="header text-black font-bold mb-5">Latest News</h1>
       </section>
 
       <hr />
 
-      <section>
-        <h1>Become A Member</h1>
-        <p>
+      <section class="mb-12 mt-12 px-16">
+        <h1 class="header text-black font-bold mb-5">Become A Member</h1>
+        <p class="mb-5">
           Producers, directors, cinematographers, editors, curators,
           distributors and more. They are Emmy winners, Academy members, all
           highly-respected and sought after for keynotes, master classes,
           consults, panels, juries and boards. Become part of this community.
         </p>
-        <h2>Connect with members</h2>
-        <p>
+        <h2 class="content-text-2 text-black font-semibold">
+          Connect with members
+        </h2>
+        <p class="mb-5">
           Keep track of everything at a glance with high-level, multi-project
           visibility.
         </p>
-        <h2>Learn about the work</h2>
-        <p>
+        <h2 class="content-text-2 text-black font-semibold">
+          Learn about the work
+        </h2>
+        <p class="mb-5">
           Empower your teams to work autonomously, without losing sight of
           what’s happening.
         </p>
-        <a href="#">Learn more</a>
+        <p class="mb-5"><a href="#">Learn more</a></p>
       </section>
 
       <hr />
 
-      <section>
-        <h1>FILMMAKER DIRECTORY</h1>
-        <p>
+      <section class="centered-content mb-12 mt-12 mb-5">
+        <h1 class="header text-black font-bold mb-5">FILMMAKER DIRECTORY</h1>
+        <p class="content-text-2 text-center px-5 mb-5">
           Browse through our directory of talented filmmakers of our community
         </p>
       </section>
 
       <hr />
 
-      <section>
-        <h1>FILM CATALOG</h1>
-        <p>Browse through the work of our filmmakers</p>
+      <section class="centered-content mb-12 mt-12 mb-5">
+        <h1 class="header text-black font-bold mb-5">FILM CATALOG</h1>
+        <p class="content-text-2 text-center px-5 mb-5">
+          Browse through the work of our filmmakers
+        </p>
       </section>
 
       <hr />
 
-      <section></section>
+      <section>
+        <!-- donate -->
+      </section>
+
+      <hr />
+
+      <section class="centered-content mb-12 mt-12 px-16">
+        <h1 class="header text-black font-bold mb-5">
+          Frequently Asked Questions
+        </h1>
+        <p class="content-text-2 text-center px-5 mb-5">
+          Got a question? We've got answers. If you have some other questions,
+          contact us using email.
+        </p>
+        <v-expansion-panels variant="accordion">
+          <v-expansion-panel
+            title="How do I become a member?"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          ></v-expansion-panel>
+          <v-expansion-panel
+            title="How do I list my work in film catalog"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          ></v-expansion-panel>
+          <v-expansion-panel
+            title="How can I support BDC?"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          ></v-expansion-panel>
+          <v-expansion-panel
+            title="How can I connect with leadership at BDC?"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          ></v-expansion-panel>
+        </v-expansion-panels>
+      </section>
+
+      <hr />
+
+      <section>
+        <!-- contact form-->
+      </section>
     </div>
 
     <div>
@@ -115,7 +162,29 @@ export default {
 .v-slide-group__content {
   background-color: #892482;
 }
-.title {
-  font-weight: 600;
+.header {
+  font-size: 2em;
+}
+.content-text-1 {
+  font-size: 1.2em;
+}
+.content-text-2 {
+  font-size: 1.5em;
+}
+.content-text-3 {
+  font-size: 1.5em;
+}
+.centered-content-1 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+.centered-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
