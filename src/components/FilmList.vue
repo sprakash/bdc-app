b1d19225d57e4d2d168e7fcc7be5b00e0d520299
sprakash <prakashnitza@gmail.com>
2024-02-10 12:32:58 -0500

    updating to staging api endpoint

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -17,7 +17,7 @@ export default {
     const error = ref(null);
     const getFilms = async () => {
       try {
-        const url = `http://localhost:3000/film-records`;
+        const url = `https://fyt9xw8cih.execute-api.us-east-2.amazonaws.com/staging/film-records`;
         console.log(" URL ", url);
         const response = await fetch(url);
         const data = await response.json();
