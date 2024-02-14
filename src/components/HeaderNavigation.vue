ee138a6548afdffe7d0667090aeeab32a5de65e0
sprakash <prakashnitza@gmail.com>
2024-02-14 13:42:20 -0500

    some top header management

--- src/components/HeaderNavigation.vue
+++ src/components/HeaderNavigation.vue
@@ -1,41 +1,105 @@
 <template>
-  <v-toolbar>
-    <v-img src="@/assets/hires-logo-bdc.png" alt="My Logo" width="150" />
-    <v-spacer />
-    <v-btn text to="/">Home</v-btn>
-    <v-btn text to="/filmmakers">Filmmakers</v-btn>
-    <v-btn
-      text
-      to="/films"
-      :class="{
-        'v-btn--active': route.name === 'films' || route.name === 'filmdetail',
-      }"
-      >Films</v-btn
-    >
-    <v-btn text to="/donate">Donate</v-btn>
-    <v-btn text to="/faqs">FAQs</v-btn>
-    <v-spacer />
+  <div id="topNav">
+    <v-toolbar>
+      <v-img src="@/assets/hires-logo-bdc.png" alt="My Logo" width="150" />
+      <v-spacer />
+      <v-btn text to="/">Home</v-btn>
+      <v-btn text to="/filmmakers">Filmmakers</v-btn>
+      <v-btn
+        text
+        to="/films"
+        :class="{
+          'v-btn--active':
+            route.name === 'films' || route.name === 'filmdetail',
+        }"
+        >Films</v-btn
+      >
+      <v-btn text to="/donate">Donate</v-btn>
+      <v-btn text to="/resources">Resources</v-btn>
+      <v-spacer />
 
-    <ul class="connect flex">
-      <li>fb icon</li>
-      <li>twitter icon</li>
-      <li>insta</li>
-      <li>youtube</li>
-      <li>linkedin</li>
-    </ul>
-  </v-toolbar>
+      <!--make this list of social media icons dynamic-->
+      <ul class="flex justify-stretch w-1/5">
+        <li class="px-2 py-1">
+          <a
+            href="https://www.facebook.com/bdcnewyork"
+            target="_blank"
+            class="block"
+          >
+            <font-awesome-icon
+              :icon="{
+                prefix: 'fab',
+                iconName: 'facebook',
+              }"
+              style="color: #0350a0"
+          /></a>
+        </li>
+        <li class="px-2 py-1">
+          <a
+            href="https://instagram.com/bdcnewyork"
+            target="_blank"
+            class="block"
+          >
+            <font-awesome-icon
+              :icon="{ prefix: 'fab', iconName: 'instagram' }"
+              style="color: #d31e2d"
+            />
+          </a>
+        </li>
+        <li class="px-2 py-1">
+          <a
+            href="http://www.twitter.com/bdcnewyork"
+            target="_blank"
+            class="block"
+          >
+            <font-awesome-icon
+              :icon="{ prefix: 'fab', iconName: 'twitter' }"
+              style="color: lightblue"
+            />
+          </a>
+        </li>
+
+        <li class="px-2 py-1">
+          <a
+            href="https://www.youtube.com/user/TheBDCNY/videos"
+            target="_blank"
+            class="block"
+          >
+            <font-awesome-icon
+              :icon="{ prefix: 'fab', iconName: 'youtube' }"
+              style="color: #ed5d29"
+            />
+          </a>
+        </li>
+        <li class="px-2 py-1">
+          <a
+            href="https://www.linkedin.com/company/black-documentary-collective-inc"
+            target="_blank"
+            class="block"
+          >
+            <font-awesome-icon
+              :icon="{ prefix: 'fab', iconName: 'linkedin' }"
+              style="color: lightseagreen"
+            />
+          </a>
+        </li>
+      </ul>
+    </v-toolbar>
+  </div>
 </template>
 <script>
 import { useRoute } from "vue-router";
-
 export default {
   setup() {
     const route = useRoute();
-
     return {
       route,
     };
   },
 };
 </script>
-<style scoped></style>
+<style>
+#topNav .v-btn {
+  font-size: 1.25em;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faYoutube,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 import { loadFonts } from "./plugins/webfontloader";
 import "@/index.css";
 import router from "./router";
@@ -10,15 +21,12 @@ import MasonryWall from "@yeger/vue-masonry-wall";
 import { Amplify } from "aws-amplify";
 import config from "./amplifyconfiguration.json";
 
+library.add(faFacebook, faTwitter, faInstagram, faYoutube, faLinkedin);
+
 //github.com/sgoodz/vue3-instagram-feed
 import InstagramFeed from "vue3-instagram-feed";
 
-//https://github.com/antonreshetov/vue-unicons?tab=readme-ov-file
-import Unicon from "vue-unicons/dist/vue-unicons-vue2.umd";
-import { uniLayerGroupMonochrome, uniCarWash } from "vue-unicons/dist/icons";
-
 Amplify.configure(config);
-Unicon.add([uniLayerGroupMonochrome, uniCarWash]);
 
 loadFonts();
 
@@ -30,5 +38,4 @@ app.use(router); // Use the router instance directly
 app.use(pinia);
 app.use(MasonryWall);
 app.use(InstagramFeed);
-app.use(Unicon);
-app.mount("#app");
+app.component("font-awesome-icon", FontAwesomeIcon).mount("#app");
